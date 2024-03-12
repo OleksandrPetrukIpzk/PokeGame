0d41f95ebe9f5608ce3a6afbf1bc1940715d86da
Sanya__Petruk <oleksandr.petruk@infopulse.com>
2024-03-12 14:07:45 +0200

    fix(achives): fix bug with achives

--- src/functions/achives.ts
+++ src/functions/achives.ts
@@ -7,8 +7,8 @@ import UserServices from "@/services/userServices";
 
 export const addAchives = async (userId: string, key:  keyof typeof ACHIEVEMENT, count: number, dispatch: Dispatch<AnyAction>, ids: number[], text: string, functions: Function) =>{
     const index = ACHIEVEMENT[key].findIndex((item) => !ids.includes(item.id) && item.count > count);
-    const id = ACHIEVEMENT[key][index].id;
     if(index >= 0){
+        const id = ACHIEVEMENT[key][index].id;
         dispatch(functions(count + 1))
         if(count + 1 === ACHIEVEMENT[key][index].count){
             youHaveAchive(text +  Number(count + 1) + ' times')
